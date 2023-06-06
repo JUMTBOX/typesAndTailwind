5e997de0559918fe447883e16793413e5e3e5ab5
JUMTBOX <yjey12@naver.com>
2023-06-06 15:44:31 +0900

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-// import JoinBtn from "./components/JoinBtn";
+import JoinBtn from "./components/JoinBtn";
 import Rehab from "./components/Rehab";
 
 function App() {
   return (
     <div className="App" style={{ textAlign: "center" }}>
       <Rehab />
+      <JoinBtn />
     </div>
   );
 }
